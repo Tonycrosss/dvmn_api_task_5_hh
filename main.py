9ff5244e7589f1fd9b9f7e99b3f320cbe5dd7723
mpak <starovoitov.anton@gmail.com>
2019-03-05 10:52:15 +0500

    full refactor

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from terminaltables import AsciiTable
 import logging
 import argparse
 
+
 VERBOSITY_TO_LOGGING_LEVELS = {
     0: logging.WARNING,
     1: logging.INFO,
@@ -22,7 +23,7 @@ def get_predict_salary(sallaries):
         else:
             continue
         predicted_salary_list.append(predicted_salary)
-    if predicted_salary_list != []:
+    if predicted_salary_list:
         processed_salaries = len(predicted_salary_list)
         average_salary = sum(predicted_salary_list) // len(
             predicted_salary_list)
@@ -31,27 +32,7 @@ def get_predict_salary(sallaries):
     return processed_salaries, average_salary
 
 
-def get_predict_rub_salary_hh(vacancy):
-    all_salaries = []
-    for item in vacancy['items']:
-        if item is not None and item['salary'] is not None and \
-                item['salary']['currency'] == 'RUR':
-            salary = item['salary']
-            all_salaries.append(salary)
-    return all_salaries
-
-
-def get_predict_rub_salary_sj(vacancy):
-    all_salaries = []
-    for item in vacancy['objects']:
-        # унифицируем ключи
-        item["from"] = item.pop("payment_from")
-        item["to"] = item.pop("payment_to")
-        all_salaries.append(item)
-    return all_salaries
-
-
-def get_salaries_hh(language, region, period):
+def get_vacancies_hh(language, region, period=None):
     url = "https://api.hh.ru/vacancies"
     params = {
         "area": 1,
@@ -62,17 +43,19 @@ def get_salaries_hh(language, region, period):
     }
     response = requests.get(url, params=params)
     response_json = response.json()
+    founded_vanacies = response_json['found']
+    vacancies = response_json['items']
     pages = response_json['pages']
     for page in range(2, pages):
         logging.info(f'Loading  {language} page : {page}....')
         params["page"] = page
-        response = requests.get(url, params=params)
-        for item in response.json()['items']:
-            response_json['items'].append(item)
-    return response_json
+        sallaries = requests.get(url, params=params)
+        for item in sallaries.json()['items']:
+            vacancies.append(item)
+    return founded_vanacies, vacancies
 
 
-def get_salaries_sj(language, region, period=None):
+def get_vacancies_sj(language, region, period=None):
     secret_key = "v3.r.128977974.a1abb3a24d8881daebd4b8268c0bb09839c95ca9.6c1f43473acc583d623876c00b4f6f535ede39dd"
     url = "https://api.superjob.ru/2.0/vacancies/"
     headers = {
@@ -84,19 +67,31 @@ def get_salaries_sj(language, region, period=None):
     }
     response = requests.get(url, headers=headers, params=params)
     response_json = response.json()
-    return response_json
+    founded_vacancies = response_json['total']
+    vacancies = response_json['objects']
+    return founded_vacancies, vacancies
 
 
-def collect_hh_stat(json):
-    founded_vacancies = len(json)
-    processed_salaries_hh, hh_av_salary = get_predict_salary(json)
-    return founded_vacancies, processed_salaries_hh, hh_av_salary
+def get_only_rub_av_salary_hh(vacancies):
+    all_salaries = []
+    for item in vacancies:
+        if item is not None and item['salary'] is not None and \
+                item['salary']['currency'] == 'RUR':
+            salary = item['salary']
+            all_salaries.append(salary)
+    processed_salaries, average_salary = get_predict_salary(all_salaries)
+    return processed_salaries, average_salary
 
 
-def collect_superjob_stat(json):
-    founded_vacancies = len(json)
-    processed_salaries_superjob, superjob_av_salary = get_predict_salary(json)
-    return founded_vacancies, processed_salaries_superjob, superjob_av_salary
+def get_only_rub_av_salary_sj(vacancies):
+    all_salaries = []
+    for item in vacancies:
+        # унифицируем ключи
+        item["from"] = item.pop("payment_from")
+        item["to"] = item.pop("payment_to")
+        all_salaries.append(item)
+    processed_salaries, average_salary = get_predict_salary(all_salaries)
+    return processed_salaries, average_salary
 
 
 def main():
@@ -105,35 +100,36 @@ def main():
     args = parser.parse_args()
     logging_level = VERBOSITY_TO_LOGGING_LEVELS[args.verbose]
     logging.basicConfig(level=logging_level)
-    top_langs_list = ["Программист Python", "Программист Ruby",
+    top_lang_list = ["Программист Python", "Программист Ruby",
                       "Программист Java",
                       "Программист Swift", "Программист Javascript",
                       "Программист Go",
                       "Программист C++", "Программист PHP", "Программист C#"]
-    table_data_hh = table_data_superjob = (('Наименование вакансии',
+    table_data_hh = table_data_sj = (('Наименование вакансии',
                                             'Всего найдено вакансий',
                                             'Обработано вакансий',
                                             'Средняя зарплата'),)
-    for lang in top_langs_list:
-        hh_json = get_salaries_hh(lang, 113, 30)
-        hh_sallaries_list = get_predict_rub_salary_hh(hh_json)
-        sj_json = get_salaries_sj(lang, 4)
-        sj_sallaries_list = get_predict_rub_salary_sj(sj_json)
+    for language in top_lang_list:
+        hh_founded_vacancies_quantity, hh_vacancies = get_vacancies_hh(language, 113, 30)
+        hh_processed_salaries, hh_average_salary = get_only_rub_av_salary_hh(hh_vacancies)
         table_data_hh = table_data_hh + \
-                        ((lang, *collect_hh_stat(hh_sallaries_list)),)
-        table_data_superjob = table_data_superjob + ((lang, *collect_superjob_stat(sj_sallaries_list)),)
+                        ((language, hh_founded_vacancies_quantity, hh_processed_salaries, hh_average_salary),)
 
+
+
+        sj_founded_vacancies_quantity, sj_vacancies = get_vacancies_sj(language, 4)
+        sj_processed_salaries, sj_average_salary = get_only_rub_av_salary_sj(
+            sj_vacancies)
+        table_data_sj = table_data_sj + \
+                        ((language, sj_founded_vacancies_quantity,
+                          sj_processed_salaries, sj_average_salary),)
     hh_title = 'HH Moscow'
-    superjob_title = 'SJ Moscow'
     table_instance_hh = AsciiTable(table_data_hh, hh_title)
-    table_instance_superjob = AsciiTable(table_data_superjob, superjob_title)
+    sj_title = 'SJ Moscow'
+    table_instance_sj = AsciiTable(table_data_sj, sj_title)
     print(table_instance_hh.table)
-    print(table_instance_superjob.table)
+    print(table_instance_sj.table)
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
